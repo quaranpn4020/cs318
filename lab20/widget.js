b8c57b62da89d6a274311ea242a0392b2cee1da6
quaranpn4020 <quaranpn4020@uwec.edu>
2017-04-19 14:20:20 -0500

    lab21 submit

--- lab20/widget.js
+++ lab20/widget.js
@@ -5,8 +5,10 @@ function move() {
         isRunning = true;
         var w = $(window).width();
         var h = $(window).height();
-        var randomLeft = Math.floor(Math.random() * w + 1);
-        var randomTop = Math.floor(Math.random() * h + 1);
+        var dW = $("#widget").width();
+        var dH = $("#widget").height();
+        var randomLeft = Math.floor(Math.random() * (w - dW) + 1);
+        var randomTop = Math.floor(Math.random() * (h - dH) + 1);
         $("#widget").animate({
             left: randomLeft,
             top: randomTop
