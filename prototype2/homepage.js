22c06707baf2008f65301a9df0f724dca3f13635
Peter Quaranta <quaranpn4020@uwec.edu>
2017-05-02 22:25:42 -0500

    proto2 update

--- prototype2/homepage.js
+++ prototype2/homepage.js
@@ -1,14 +1,37 @@
 var changeTime = 4000;
-var waitTime = 5000;
+var waitTime = 9000;
+var imagesInSlide = 3;
+var current = 0;
 
 $(document).ready(
-    function() {
-        setTimeout(
-            function testalert() {
-                $("#first").hide().fadeIn(changeTime);
-                $("#second").fadeOut(changeTime);
-            }, waitTime
-        );
+    function runSS() {
+        // First find urls
+        var indexOne = current;
+        current++;
+        if (current > imagesInSlide) {
+            current = 1;
+        }
+        var indexTwo = current;
+
+        var urlOne = "slideshowimages/test" + indexOne + ".jpg";
+        var urlTwo = "slideshowimages/test" + indexTwo + ".jpg";
+
+        // Set background images    
+        $("#first").css('background-image', 'url(' + urlOne + ')');
+        $("#first").show();
+
+        // Hide second for toggle
+        $("#second").hide();
+
+        // Set second
+        $("#second").css('background-image', 'url(' + urlTwo + ')');
+
+        $("#first").fadeToggle(changeTime);
+        $("#second").fadeToggle(changeTime);
+
+        setTimeout(function callAgain() {
+            runSS();
+        }, changeTime + waitTime);
         
         
     }
